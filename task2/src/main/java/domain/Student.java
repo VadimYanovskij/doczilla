cdc16cfb70aef371f5881a6991892bd4a442c8e8
Vadim Yanovskij <garretnmru@gmail.com>
2022-05-21 17:44:44 +0300

    task2: add domain class Student

---
+++ task2/src/main/java/domain/Student.java
@@ -0,0 +1,64 @@
+package domain;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String lastname;
+    private String middleName;
+    private LocalDate birthday;
+    private int group;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+}
